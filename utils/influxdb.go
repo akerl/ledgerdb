87573a1287aae43d42fa2dcad0b4e45f2c44674e
Les Aker <me@lesaker.org>
2024-12-01 21:39:05 -0500

    add influx for real

---
+++ utils/influxdb.go
@@ -0,0 +1,10 @@
+package utils
+
+import (
+	"github.com/akerl/ledgerdb/config"
+)
+
+// WriteInflux loads transactions into Influx
+func WriteInflux(_ config.Config, _ []Transaction) error {
+	return nil
+}
